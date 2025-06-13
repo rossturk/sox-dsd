4a8ef2b0e90c4de5177a8a02a877c9538b67b5f1
Ross Turk <ross@rossturk.com>
2025-06-13 03:52:48 +0000

    test
    Signed-off-by: Ross Turk <ross@rossturk.com>

--- apply-patches.sh
+++ apply-patches.sh
@@ -3,8 +3,8 @@
 
 set -e
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$SCRIPT_DIR"
+#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#cd "$SCRIPT_DIR"
 
 # config
 PATCH_DIR="patches"
